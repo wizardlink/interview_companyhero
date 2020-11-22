84ee39d214c31ed3c4a4faf03330c147fc16eb3c
Alexandre Cavalheiro S. Tiago Da Silva <alexandre.cssilva@gmail.com>
2020-11-21 21:17:16 -0300

    fix: properly load ant.design

--- next.config.js
+++ next.config.js
@@ -1,10 +1,40 @@
 const withLess = require("@zeit/next-less");
+const lessToJS = require("less-vars-to-js");
+const fs = require("fs");
+const path = require("path");
+
+// Custom ant.design configuration.
+const themeVariables = lessToJS(
+	fs.readFileSync(path.resolve(__dirname, "./assets/antd-custom.less"), "utf8"),
+);
 
 /* Required to be able to work with LESS. */
 module.exports = withLess({
-	cssModules: true,
+	lessLoaderOptions: {
+		javascriptEnabled: true,
+		modifyVars: themeVariables, // Apply custom config.
+	},
 	webpack: (config) =>
 	{
+		const antStyles = /antd\/.*?\/style.*?/;
+		const origExternals = [...config.externals];
+
+		config.externals = [
+			(context, request, callback) =>
+			{
+				if (request.match(antStyles)) return callback();
+				if (typeof origExternals[0] === "function")
+				{
+					origExternals[0](context, request, callback);
+				}
+				else
+				{
+					callback();
+				}
+			},
+			...(typeof origExternals[0] === "function" ? [] : origExternals),
+		];
+
 		config.module.rules.push({
 			test: /\.(png|jpg)$/,
 			use: {
